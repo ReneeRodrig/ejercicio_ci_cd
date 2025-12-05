ba1821b2793406b710930e2bdad036abcdde971b
ReneeRodrig <renerodrigalonso@gmail.com>
2025-12-05 09:19:38 +0100

    fixing github actions 3

--- src/main.py
+++ src/main.py
@@ -1,2 +1,8 @@
 def multiply(num1, num2):
     return num1 * num2
+
+def to_upper(text):
+    return text.upper()
+
+def to_lower(text):
+    return text.lower()
--- test/test_main.py
+++ test/test_main.py
@@ -1,5 +1,11 @@
-from src.main import multiply
+from src.main import multiply, to_upper, to_lower
 
 def test_multiply():
     assert multiply(2,3) == 6
     assert multiply(0,4) == 0
+    
+def test_to_upper():
+    assert to_upper('Renee') == 'RENEE'
+    
+def test_to_lower():
+    assert to_lower('Renee') == 'renee'
